8c22083d16bf6e109c4b13b4cbfc5c8e81e54f27
chaudhuree <chaudhuree00@gmail.com>
2022-12-11 14:10:44 +0600

    docs: finish with dbms  and handle error of duplicate key error

--- controllers/userContorller.js
+++ controllers/userContorller.js
@@ -1,12 +1,18 @@
 const ErrorHandler = require('../utils/errorHandelers')
+const User = require('../model/user')
 
-const userController = (req, res, next) => {
-  const isExist = true
-  if (isExist) {
-    return next(new ErrorHandler(401, 'user already exists'))
+const userController = async (req, res, next) => {
+  try {
+    // const user = await User.findOne({ email: "chaudhuree@gmail.com" });
+    // if (user) {
+    //   return next(new ErrorHandler(401, 'user already exists'))
+    // }
+    await User.create({ name: "sohan", email: "sohan@gmail.com" })
+
+    res.status(200).json({ success: true, message: 'user created successfully' })
+  } catch (error) {
+    next(new ErrorHandler(500,error.message))
   }
-
-  res.status(200).json({ success: true, message: 'user created successfully' })
 
 }
 
--- middlewares/Error.js
+++ middlewares/Error.js
@@ -1,7 +1,12 @@
 const Error=(err, req, res, next) => {
   err.statusCode=err.statusCode||500
   err.message=err.message || 'internal server error'
+console.log({err});
+  if(err.code=== 11000){
+    err.statusCode=400,
+    err.message="Duplicate key error"
+  }
 
-  res.status(err.statusCode).json({message:err.message})
+  res.status(err.statusCode).json({statuscode:err.statusCode,message:err.message})
 }
 module.exports =Error
